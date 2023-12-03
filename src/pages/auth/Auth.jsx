5f042b45ae2031d00aa8b16ec14560bc840f1e72
fhilola <hilolafayoziddinova14@gmail.com>
2023-12-03 14:32:53 +0500

    auth style added

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Outlet, NavLink, Link } from 'react-router-dom'
 import './Auth.scss'
 
 const Auth = () => {
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    navigate('/auth/login')
+  },[])
   return (
     <div className='auth'>
+      <Link to={'/'}>
+      <h1>ERP AUTH</h1>
+      </Link>
       <div className="auth-form__wrapper">
-        <nav>
-          <ul>
-            <li>
-              <NavLink>
-                
+        <nav className='auth__nav'>
+          <ul className='nav__list'>
+            <li className='list__item'>
+              <NavLink to={'/auth/login'} className={({isActive}) => isActive ? 'link link--active' : 'link'}>
+                Login
               </NavLink>
             </li>
-            <li>
-              <NavLink>
-
+            <li className='list__item'>
+              <NavLink to={'/auth/register'} className={({isActive}) => isActive ? 'link link--active' : 'link'}>
+                Register
               </NavLink>
             </li>
           </ul>
--- src/routes/register/Register.jsx
+++ src/routes/register/Register.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button } from "../../utils"
 
 const Register = () => {
   return (
-    <div>
+    <div className='auth-form-container'>
       <h2>Register</h2>
+      <form className='auth-form'>
+        <input type="text" placeholder='Your Name' />
+        <input type="email" placeholder='Your Email' />
+        <input type="password" placeholder='Your Password' />
+        <Button text={'Register'} isloading={false} type={'submit'} appearence={'success'}/>
+      </form>
     </div>
   )
 }
